0265570b102fd47a553328e89681af59056d058c
Denis Shadrin <deshaser@gmail.com>
2014-03-05 22:21:34 +0400

    hello world

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ToDo</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="styles.css">
+        <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.12/angular.min.js"></script>
+        <script src="scripts.js" type="text/javascript"></script>
+    </head>
+    <body>
+        Hello world
+    </body>
+</html>
