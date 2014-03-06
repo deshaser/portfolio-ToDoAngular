d8759fe8110f39b3d41657cd077e35105420ecda
Denis Shadrin <deshaser@gmail.com>
2014-03-06 22:33:07 +0400

    base js functional

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html ng-app>
     <head>
         <title>ToDo</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="static/styles.css">
         <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.12/angular.min.js"></script>
-        <script src="scripts.js" type="text/javascript"></script>
+        <script src="static/scripts.js" type="text/javascript"></script>
     </head>
     <body>
-        Hello world
+        <h2>todos</h2>
+        <div ng-controller="TodoCtrl">
+            <form ng-submit="addTodo()">
+                <input type="text" ng-model="todoText"  size="30" placeholder="What needs to be done?">
+                <input class="btn-primary" type="submit" value="add">
+            </form>
+            <ul class="unstyled">
+                <li ng-repeat="todo in todos">
+                    <input type="checkbox" ng-model="todo.done">
+                    <span class="done-{{todo.done}}">{{todo.text}}</span>
+                </li>
+            </ul>
+            <span>{{remaining()}} item left</span>
+            <a href="" ng-click="archive()">Clear completed({{todos.length - remaining()}})</a>
+        </div>
     </body>
 </html>
