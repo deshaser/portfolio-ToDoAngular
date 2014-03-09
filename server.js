9c9ed1c7ab855feea66894c647a492199ea4641d
Denis Shadrin <deshaser@gmail.com>
2014-03-09 23:06:03 +0400

    moved on node.js, connected socket.io, added function added new todo via socket

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+})
+app.use(express.static(__dirname + '/static'));
+
+io.sockets.on('connection', function(socket){
+    socket.on('send todo', function(data) {
+        console.log(data)
+        io.sockets.emit('new todo', data);
+    });
+});
--- static/scripts.js
+++ static/scripts.js
@@ -1,18 +1,25 @@
-function TodoCtrl($scope) {
+var todoApp = angular.module('TodoApp', ['ngSocket']);
+
+todoApp.controller('TodoCtrl', function($scope, $socket) {
 
     // function initializes todo's
     $scope.todos = [
         {id: 1, text:'learn angular', done:true},
         {id: 2, text:'build an angular app', done:false}];
 
-    // function added todo in todo's array
+    // function send todo in server
     $scope.addTodo = function() {
         if ($scope.todoText) {
-            var $id = $scope.todos.length ? $scope.todos[$scope.todos.length-1].id + 1 : 1; // in the future get this value into php
-            $scope.todos.push({id: $id, text:$scope.todoText, done:false});
+            var $id = $scope.todos.length ? $scope.todos[$scope.todos.length-1].id + 1 : 1; // in the future get this value into server
+            $socket.emit('send todo', {id: $id, text:$scope.todoText, done:false});
             $scope.todoText = '';
         }
     };
+
+    // functon awaiting data from the server and added new todo
+    $socket.on('new todo', function(data) {
+        $scope.todos.push({id: data.id, text: data.text, done: data.done});
+    });
 
     // function returns the number of active todo's
     $scope.remaining = function() {
@@ -38,4 +45,4 @@ function TodoCtrl($scope) {
         $scope.todos = _.reject($scope.todos, function(todo){ return todo.id == id; });
     };
 
-}
+});
