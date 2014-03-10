1e10b6e30af5416a3174f3094573b4cb41ead0e4
Denis Shadrin <deshaser@gmail.com>
2014-03-10 15:14:24 +0400

    require mongoDB, included module mongoos and insert demo data in db

---
+++ libs/mongoose.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/todoangular');
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.log('connection error:', err.message);
+});
+db.once('open', function callback() {
+    console.log('Connected to DB');
+});
+
+var todoSchema = mongoose.Schema({
+    text: String,
+    done: Boolean
+})
+var Todos = mongoose.model('Todos', todoSchema)
+
+// if db is empty - add new todo's
+Todos.find(function(err, data){
+    if (data && !data[0]) {
+        // init demo todo's
+        var demoTodos = [
+            { text: 'something', done: false },
+            { text: 'everything', done: true }
+        ];
+        var todoOne = [];
+        // save in db all demo todo's
+        for (var i = 0; i < demoTodos.length; i++) {
+            todoOne[i] = new Todos(demoTodos[i]);
+            todoOne[i].save();
+        }
+    };
+});
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+    io = require('socket.io').listen(server),
+    db = require('./libs/mongoose');
 
 server.listen(3000);
 
